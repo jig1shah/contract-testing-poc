a988235f4d598ca40c705d4208df0a356af74364
jig1shah <shahj@aljazeera.net>
2024-05-07 19:17:51 +0530

    Updated code for Opinion

--- src/consumer.spec.js
+++ src/consumer.spec.js
@@ -16,81 +16,6 @@ const provider = new PactV3({
 });
 
 describe("API Pact test", () => {
-  describe("Getting Data from GQL", () => {
-    // test("Breaking News Exists For Website", async () => {
-    //   await provider.addInteraction({
-    //     states: [{ description: "Breaking News Exists For Website" }],
-    //     uponReceiving: "Get All Breaking News For Website",
-    //     withRequest: {
-    //       method: "GET",
-    //       path: "/graphql",
-    //       query: {
-    //         "wp-site": "aje",
-    //         operationName: "ArchipelagoBreakingTickerQuery",
-    //         variables: "{}",
-    //         extensions: "{}",
-    //       },
-    //       headers: {
-    //         "wp-site": "aje",
-    //       }
-    //     },
-    //     willRespondWith: {
-    //       status: 200,
-    //       headers: {
-    //         "Content-Type": "application/json; charset=utf-8",
-    //       },
-    //       body: {
-    //         data: {
-    //           breakingNews: {
-    //             post: like("2840262"),
-    //             tickerTitle: like("breaking"),
-    //             tickerText: like([
-    //               "New topics",
-    //               "New breaking news",
-    //               "New update",
-    //             ]),
-    //             modified: like("2024-04-23 08:25:30"),
-    //             link: like(
-    //               "/news/2024/4/21/mass-grave-with-50-bodies-found-in-khan-younis-hlstyleguide-2"
-    //             ),
-    //             __typename: like("BreakingNews"),
-    //           },
-    //         },
-    //       },
-    //     },
-    //   });
-
-    //   await provider.executeTest(async (mockService) => {
-    //     // Request to moke server
-    //     const news = await axios
-    //       .get(`${mockService.url}/graphql`, {
-    //         params: {
-    //           "wp-site": "aje",
-    //           operationName: "ArchipelagoBreakingTickerQuery",
-    //           variables: "{}",
-    //           extensions: "{}",
-    //         },
-    //         headers: {
-    //           "wp-site": "aje",
-    //         }
-    //       })
-    //       .then((r) => r.data);
-
-    //     expect(news).toStrictEqual({
-    //       data: {
-    //         breakingNews: {
-    //           post: "2840262",
-    //           tickerTitle: "breaking",
-    //           tickerText: ["New topics", "New breaking news", "New update"],
-    //           modified: "2024-04-23 08:25:30",
-    //           link: "/news/2024/4/21/mass-grave-with-50-bodies-found-in-khan-younis-hlstyleguide-2",
-    //           __typename: "BreakingNews",
-    //         },
-    //       },
-    //     });
-    //   });
-    // });
-
     test("Opinion Exists For Website", async () => {
       await provider.addInteraction({
         states: [{ description: "Opinion Exists For Website" }],
@@ -1663,4 +1588,3 @@ describe("API Pact test", () => {
       });
     });
   });
-});
